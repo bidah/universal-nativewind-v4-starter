529cf4a98eccb05090358fd7c0495a3f2985953a
ROFI <rodrigofigueroa.name@gmail.com>
2024-10-07 14:44:50 -0300

    update to expo 51 and nativewind 4.1.10

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,19 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      [
-        "babel-preset-expo",
-        {
-          jsxImportSource: "nativewind",
-        },
-      ],
-      "nativewind/babel",
-    ],
-    plugins: [
-      // Required for expo-router
-      "expo-router/babel",
-      "@babel/plugin-proposal-export-namespace-from",
-      "react-native-reanimated/plugin",
-    ],
-  };
+module.exports = (api) => {
+	api.cache(true);
+	return {
+		presets: [
+			[
+				"babel-preset-expo",
+				{
+					jsxImportSource: "nativewind",
+				},
+			],
+			"nativewind/babel",
+		],
+		plugins: [
+			// Required for expo-router
+			"@babel/plugin-proposal-export-namespace-from",
+			"react-native-reanimated/plugin",
+		],
+	};
 };
